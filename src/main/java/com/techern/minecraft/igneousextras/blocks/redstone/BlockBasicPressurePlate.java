44b95aa08622db9f6ad9ba35a1ed3d6dac5894cd
Cruz Julian Bishop <cruzjbishop@gmail.com>
2016-04-04 08:06:13 +1000

    Add BlockBasicPressurePlate

---
+++ src/main/java/com/techern/minecraft/igneousextras/blocks/redstone/BlockBasicPressurePlate.java
@@ -0,0 +1,25 @@
+package com.techern.minecraft.igneousextras.blocks.redstone;
+
+import net.minecraft.block.BlockPressurePlate;
+import net.minecraft.block.material.Material;
+
+/**
+ * An extension of {@link net.minecraft.block.BlockPressurePlate} for use in Igneous Extras
+ *
+ * @since 0.0.2
+ */
+public class BlockBasicPressurePlate extends BlockPressurePlate {
+
+    /**
+     * Creates a new {@link BlockBasicPressurePlate}
+     *
+     * @param material The {@link Material} used by this {@link BlockBasicPressurePlate}
+     * @param sensitivity The {@link net.minecraft.block.BlockPressurePlate.Sensitivity} used by this {@link BlockBasicPressurePlate}
+     *
+     * @since 0.0.2
+     */
+    public BlockBasicPressurePlate(Material material, Sensitivity sensitivity) {
+        super(material, sensitivity);
+    }
+
+}
